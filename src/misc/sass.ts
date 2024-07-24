f6dea9d3392ab4835040daf5d1e52acfd974dbf2
Kurt Lourens <kurt.lourens@outlook.com>
2024-07-24 22:36:00 +0200

    💚 Fix build

--- src/misc/sass.ts
+++ src/misc/sass.ts
@@ -1,10 +1,14 @@
 import fs from 'fs/promises';
 import sass from 'sass';
 
+import { createFoldersOfDestFilePath } from 'helpers/fileHelper';
+
 export const generateMainCss = () => {
   const cssContent = sass.compile('src/scss/main.scss');
-  fs.writeFile('public/assets/css/main.css', cssContent.css);
+  const destFile = 'public/assets/css/main.css';
+  createFoldersOfDestFilePath(destFile);
+  fs.writeFile(destFile, cssContent.css);
   if (cssContent.sourceMap != null) {
-    fs.writeFile('public/assets/css/main.css.map', JSON.stringify(cssContent.sourceMap));
+    fs.writeFile(destFile.replace('.css', '.css.map'), JSON.stringify(cssContent.sourceMap));
   }
 };
